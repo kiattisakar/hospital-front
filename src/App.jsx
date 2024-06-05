115179a819f294addd44274422191b54632baf61
Wissarut_Chuakam <wissarut.ch@rmuti.ac.th>
2024-06-05 15:20:55 +0700

    testLastest

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
-
-import './App.css'
+import React from 'react'
+import Login from './login/Login.jsx'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-xl font-bold text-red-500'>test</h1>
-      <h1 className='text-xl font-bold text-red-500'>2222</h1>
-      <h1 className='text-xl font-bold text-red-500'>test</h1>
-      <h1 className='text-xl font-bold text-red-500'>2222</h1>
-      <h1 className='text-xl font-bold text-red-500'>2222</h1>
-      <h1 className='text-xl font-bold text-red-500'>2222</h1>
-      <h1 className='text-xl font-bold text-red-500'>2222</h1>
-      <h1 className=' text-green '>wissrut</h1>
+    <Login />
     </>
   )
 }
