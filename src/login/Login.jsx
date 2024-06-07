d9d3a139230f51e7e13ce7a6d39b5d26c7edddb3
Wissarut_Chuakam <wissarut.ch@rmuti.ac.th>
2024-06-07 16:00:07 +0700

    LoginV1.5.1

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import backgroundImage from '../../img/Background.png';
+import backgroundImage from '../../img/Background1.png';
+import kkhLogo from '../../img/KKHHOSPITAL.jpg';
 import CardSwitcher from './SelectRoom';
 
 export default function login() {
@@ -11,8 +12,10 @@ export default function login() {
     height: '100vh',
     width: '100%',
   };
+
   return (
     <div style={backgroundStyle}>
+      <img src={kkhLogo} alt="ตราโรงบาล" className="w-26 h-24 m-8 fixed" />
       <div className="h-screen flex ml-44">
         <CardSwitcher />
       </div>
